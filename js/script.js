628f482dbb8bb2e37310ed58d481c33b426b1e92
Michael <mishanhzer@gmail.com>
2023-11-06 13:53:58 +0500

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const tabContent = document.querySelectorAll('.tabcontent');
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  const hideTabContent = () => {
+    tabContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+      // item.style.display = 'none';
+    })
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    })
+  };
+
+
+  const showTabContent = (i = 0) => {
+    tabContent[i].classList.add('show', 'fade');
+    tabContent[i].classList.remove('hide');
+    // tabContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+  };
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      })
+    }
+  })
+
+
+
+
 
 })
-
 
 
 
