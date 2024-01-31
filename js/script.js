af3f99625e8ff6119b2f5fdafa2115f2dc358aa9
Michael <mishanhzer@gmail.com>
2024-01-31 09:12:13 +0500

    add navigation for slider

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	const modalTimerId = setTimeout(openModal, 50000);
+	// const modalTimerId = setTimeout(openModal, 50000);
 
 	function showModalByScroll() {
 		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -441,83 +441,356 @@ const getResource = async (url) => { // создаем функцию получ
 		}
 	}
 
-	// simple Slider
+	// simple Slider Моя первая практика со слайдером (сделал сам полностью)
 
-	const parentSlider = document.querySelector('.offer__slider-wrapper');
-	const imageSlider = document.querySelectorAll('.offer__slide');
+// Первый мой вариант
 
-	let spanCurrent = document.querySelector('span#current');
-	let spanTotal = document.querySelector('span#total');
+	// const parentSlider = document.querySelector('.offer__slider-wrapper'); // !PS: Родитель картинок слайдов оказался не нужен
+	// const imageSlider = document.querySelectorAll('.offer__slide'); // Получение массива слайдеров (картинок)
 
-	const sliderBtn = document.querySelector('.offer__slider-counter');
-	let counter = 1;
+	// let spanCurrent = document.querySelector('span#current'); // получаем элемент текущего слайда
+	// let spanTotal = document.querySelector('span#total'); // получаем элемент количества всех слайдов
+
+	// const sliderBtn = document.querySelector('.offer__slider-counter'); // Получение родителя кнопок
+	// let counter = 1; // создаем счетчик
+
+
+	// sliderBtn.addEventListener('click', (e) => { // навешиваем обработчик события на родителя кнопок
+	// 	const target = e.target;
+	// 	counterIndex(target);
+	// });
+
+	// function counterIndex(target) { // создаем функцию, которая меняет счетчик слайдов
+	// 	if (target && target.classList.contains('offer__slider-next') || target.classList.contains('img__slider-next')) { // делаем условие, делигируем
+	// 		if (spanCurrent.innerText < spanTotal.innerText) { // условие (если кол-во слйдов меньше чем общее кол-во слайдов)
+	// 			spanCurrent.textContent = `0${++counter}`;
+	// 		} else { // иначе (начинаем счетчик заново)
+	// 			counter = 0;
+	// 			spanCurrent.textContent = `0${++counter}`;
+	// 		}
+
+	// 		nextImageSlider(spanCurrent.textContent);
+	// 	} 
+
+	// 	if (target && target.classList.contains('offer__slider-prev') || target.classList.contains('img__slider-prev')) { // делаем аналогчно выше, только в обратную сторону
+	// 		if (spanCurrent.innerText <= 1) {
+	// 			counter = 5;
+	// 			spanCurrent.textContent = `0${--counter}`;
+	// 		} else {
+	// 			spanCurrent.textContent = `0${--counter}`;
+	// 		}
+
+	// 		nextImageSlider(spanCurrent.textContent);
+	// 	} 
+	// }
+
+
+	// function nextImageSlider(element) { // создаем функцию на получение следующей картинки в слайде
+	// 	imageSlider.forEach((image, i) => { // перебираем картинки со слайдами
+	// 		if (element - 1 === i) { // если элемент (элемент текущего слайда) будет равен индексу
+	// 			image.classList.add('show');
+	// 			image.classList.remove('hide');
+	// 		} else {
+	// 			image.classList.add('hide');
+	// 		}
+	// 	})
+	// }
+
+
+
+	// function defaultImage(el, i) { // создаем функцию на получение дефолтной картинки и счетчика
+	// 	imageSlider.forEach((item,i) => { // перебор слайдов
+	// 		if (i === 0) { // если индекс = 0
+	// 			spanCurrent.textContent = `0${1}`; // вставляем на страницу счетчик 01
+	// 			item.classList.add('show'); // показываем первую картинку слайда
+	// 			item.classList.remove('hide');
+	// 		} else {
+	// 			item.classList.add('hide');
+	// 		}
+	// 	});
+	// }
+	// defaultImage();
+
+
+	// Второй мой вариант (после отдыха)
+
+	// let sliderImg = document.querySelectorAll('.offer__slide'); // Получение блоков с картинками слайдера
+	
+	// let sliderBtn = document.querySelector('.offer__slider-counter'); // Получение родителя для кнопок
+	
+	// let currentSlider = document.querySelector('#current'); // Получение текущего количества слайдов
+	// let totalSlider = document.querySelector('#total'); // Получение общего количества слайдов
+	// let counter = 1; // Создание счетчика
+
+	// sliderBtn.addEventListener('click', (e) => { // Навешиваем обработчик события на родителя кнопок
+	// 	let target = e.target;
+	// 	if (target && target.classList.contains('offer__slider-next') || target.classList.contains('img__slider-next')) { // Делигируем на кнопку следующего слайда (Используем оператор ИЛИ, потому что одновременно на два элемента тыкнуть мы не можем)
+	// 		currentSlider.textContent = `0${++counter}`; // Добавляем на страницу прибавление счетчика
+	// 		nextElementSlider(); // Используем функцию, которая перебирает блока со слайдами и показывает нам нужный слайд
+	// 		if (counter > 4) { // Условие, если слайдов больше 4, то начинаем все заново
+	// 			counter = 0;
+	// 			currentSlider.textContent = `0${++counter}`;
+	// 			nextElementSlider();
+	// 		}
+	// 	} 
+
+	// 	// Аналогично верхнему условию, только в обратную сторону
+
+	// 	if (target && target.classList.contains('offer__slider-prev') || target.classList.contains('img__slider-prev')) { // Делигируем на кнопку предыдущего слайда
+	// 		currentSlider.textContent = `0${--counter}`;
+	// 		nextElementSlider();
+	// 		if (counter < 1) {
+	// 			counter = 5;
+	// 			currentSlider.textContent = `0${--counter}`;
+	// 			nextElementSlider();
+	// 		}
+	// 	} 
+	// });
+
+
+
+	// function nextElementSlider() { // Создаем функцию перебора слайдов
+	// 	sliderImg.forEach((item, i) => {
+	// 		if (i === counter - 1) { // если индекс i(0) будет равен счетику (0) - индекс и счетчик будет расти взависимости от условий выше
+	// 			item.classList.add('show'); // добавляем элементам класс show
+	// 			item.classList.remove('hide'); // добавляем элементам класс hide
+	// 		} else { // иначе убираем эти классы
+	// 			item.classList.add('hide');
+	// 			item.classList.remove('show');
+	// 		}
+	// 	});
+	// }
+
+
+	// function defaultImage() { // создаем функцию дефолтного изображения
+	// 	sliderImg.forEach((item, i) => {
+	// 		if (i === 0) { // если индекс = 0 
+	// 			currentSlider.textContent = `0${counter}`; // вставляем на страницу номер дефолнтго слайда
+	// 			item.classList.add('show'); // аналогично добавляем и удаляем классы
+	// 			item.classList.remove('hide');
+	// 		} else {
+	// 			item.classList.add('hide');
+	// 			item.classList.remove('show');
+	// 		}
+	// 	})
+	// }
+	// defaultImage();
+
 
 	
-	sliderBtn.addEventListener('click', (e) => {
-		const target = e.target;
-		counterIndex(target);
+	// Simple slider by Ivan Petrichenko
+
+	// const slides = document.querySelectorAll('.offer__slide'); // получаем массив всех слайдов
+	// const prev = document.querySelector('.offer__slider-prev'); // получаем кнопку предыдущего слайда
+	// const next = document.querySelector('.offer__slider-next'); // получаем кнопку следующего слайда
+
+	// const total = document.querySelector('#total'); // получаем общее количество слайдов
+	// const current = document.querySelector('#current'); // получаем текущий слайд
+
+	// let slideIndex = 1; // создаем индекс (счетчик)
+
+	// showSlides(slideIndex); // вызов функции для инициализации слайдера (чтобы показался первый слайд)
+
+	// // Условие общего количества слайдов делаем это после инициализации showSlides(slideIndex), чтобы показали общее количество слайдов (юзаем перед функцией, потому что showSlides будет вызываться вместе с обработчиком события и каждый раз когда нажимаем на кнопку будет мигать элемент с общим количеством слайдов (каждый раз общее количество слайдов будет меняться при нажатии на нопку))
+	// if (slides.length < 10) { // Условие (если количество слайдов меньше 10)
+	// 	total.textContent = `0${slides.length}`; // вставляем в индекс общего количества слайдов - 0 с количеством слайдов
+	// } else {
+	// 	total.textContent = slides.length; // если больше 10, то просто вставляем на страницу в индекс обдего количества слайдов - количество слайдов без 0
+	// }
+
+	// function showSlides(n) { // n принимает в себя slideIndex
+	// 	if (n > slides.length) { // Условие (если текущий слайд больше количества всех слайдов)
+	// 		slideIndex = 1 // то текущий слайд мы возвращаем в начало
+	// 	}
+
+	// 	if (n < 1) { // Условие (если текущий слайд меньше 1)
+	// 		slideIndex = slides.length; // то в текущий слайд мы помещаем количество всех слайдов (то есть начинмем слайдер с конца)
+	// 	}
+
+	// 	slides.forEach(item => item.style.display = 'none'); // скрываем все слайды через inline стили
+
+	// 	slides[slideIndex - 1].style.display = 'block'; // показ текущего слайда (отталкиваемся от slideIndex), отнимаем 1, чтобы получить адеватный индекс (если первый слайд, то индекс 0, если второй слайд, то индекс 1)
+
+
+	// 	// Условие, которое меняет наш счетчик и вставляем на страницу (когда работаем с текущим слайдом, юзаем внутри функции, чтобы показ слайдов соответствовал с индексом слайда)
+	// 	if (slides.length < 10) { // Условие (если количество слайдов меньше 10)
+	// 		current.textContent = `0${slideIndex}`; // вставляем в индекс текущего слайда - текущий слайд
+	// 	} else {
+	// 		current.textContent = slideIndex; // если больше 10, то просто вставляем на страницу индекс текущего слайда - текущий слайд без 0
+	// 	}
+	// }
+
+	// function plusSlides(n) { // создаем функцию, который будет прибавлять индекс
+	// 	showSlides(slideIndex += n); // если будет 1, то будет прибавляться к slideIndex 1, если -1, то будет убавляться к slideIndex 1
+	// }
+
+	// prev.addEventListener('click', () => { // создаем обработчик события на кнопку предыдущего слайда
+	// 	plusSlides(-1); // отнимаем 1 у slideIndex (то есть будет проходить все условия функции showSlides и будет показываться предыдущая картинка слайда)
+	// });
+
+	// next.addEventListener('click', () => { // создаем обработчик события на кнопку следующего слайда
+	// 	plusSlides(1); // прибавляем 1 к slideIndex (то есть будет проходить все условия функции showSlides и будет показываться следующая картинка слайда)
+	// });
+
+
+	// Difficult slider by Ivan Petrichenko (Сarousel) 
+
+	const slides = document.querySelectorAll('.offer__slide'), // получаем массив всех слайдов
+	prev = document.querySelector('.offer__slider-prev'), // получаем кнопку предыдущего слайда
+	next = document.querySelector('.offer__slider-next'), // получаем кнопку следующего слайда
+
+	total = document.querySelector('#total'), // получаем общее количество слайдов
+	current = document.querySelector('#current'), // получаем текущий слайд
+	slidesWrapper = document.querySelector('.offer__slider-wrapper'), // Получаем обертку слайдера (что за пределами ее будем скрывать)
+	slidesField = document.querySelector('.offer__slider-inner'), // Получаем карусель (будет занимать столько ширины, сколько слайдов в слайдере)
+	width = window.getComputedStyle(slidesWrapper).width; // Получаем ширину обертки слайдера (650px)
+
+	const slider = document.querySelector('.offer__slider'); // Получаем весь слайдер
+	slider.style.position = 'relative'; // ставим в позицию relative
+	const dots = []; // создаем массив (нужно поместить точки в структуру, чтобы потом работать с ней)
+
+	let slideIndex = 1; // создаем индекс (счетчик)
+	let offset = 0; // создаем (смещение)
+
+	if (slides.length < 10) { // Условие (если количество слайдов меньше 10)
+		total.textContent = `0${slides.length}`; // вставляем в индекс общего количества слайдов - 0 с количеством слайдов
+		current.textContent = `0${slideIndex}`; // вставляем в индекс текущего слайда - первый слайд
+	} else {
+		total.textContent = slides.length; // если больше 10, то просто вставляем на страницу в индекс общего количества слайдов - количество слайдов без 0
+		current.textContent = slideIndex; // если больше 10, то вставляем на страницу в индекс текущего слайда - текущий слайд без 0
+	}
+
+
+	slidesField.style.width = 100 * slides.length + '%'; // меняем ширину карусели (100 * количество слайдов) - 400%
+	slidesField.style.display = 'flex'; // Карусель делаем в флексбокс (чтобы слайды стали горизонтально)
+	slidesField.style.transition = '0.5s all'; // добавляем карусели плавный переход 
+
+	slidesWrapper.style.overflow = 'hidden'; // Все выходит за пределы обертки делаем невидимым
+
+
+	slides.forEach(slide => { // слайдам делаем фиксированную ширину в 650px (если этого не делать и без флексбокса и хиддена, ширина будет занимать все ширинку карусели - 2600px)
+		slide.style.width = width;
 	});
 
-	function counterIndex(target) {
-		if (target && target.classList.contains('offer__slider-next') || target.classList.contains('img__slider-next')) {
-			if (spanCurrent.innerText < spanTotal.innerText) {
-				spanCurrent.textContent = `0${++counter}`;
-			} else {
-				counter = 0;
-				spanCurrent.textContent = `0${++counter}`;
+
+	// Выводим повторяющийся функционал в отдельные функции
+		// Функция добавления класса активности точкам (работа с массивом)
+		function dotActive() {
+			dots.forEach(dot => dot.style.opacity = '.5'); // перебираем массив и меняем прозрачность всем точкам на 0.5
+			dots[slideIndex - 1].style.opacity = 1; // Первой точке и последующим при клике (будет менять прозрачность на 1 - на активную точку)
+		};
+
+		// Функция добавления текущего слайда на страницу
+		function currentOnPage() {
+			if (slides.length < 10) { // если количество слайдов меньше 10
+				current.textContent = `0${slideIndex}`; // То вставляем на страницу с 0
+			} else { // если меньше 10 
+				current.textContent = slideIndex; // То вставялем на сраницу без 0
 			}
+		};
 
-			nextImageSlider(spanCurrent.textContent);
-		} 
+	// Создаем обертку для наших точек
+	const dotsWrapper = document.createElement('ol'); // создаем обертку - элемент ordered-list
+	dotsWrapper.classList.add('carousel-indicators'); // добавляем ему класс
+	slider.append(dotsWrapper); // помещаем его внутрь слайдера
 
-		if (target && target.classList.contains('offer__slider-prev') || target.classList.contains('img__slider-prev')) {
-			if (spanCurrent.innerText <= 1) {
-				counter = 5;
-				spanCurrent.textContent = `0${--counter}`;
-			} else {
-				spanCurrent.textContent = `0${--counter}`;
-			}
-			previousImageSlider(spanCurrent.textContent);
-		} 
-	}
-
-
-	function nextImageSlider(element) {
-		imageSlider.forEach((image, i) => {
-			if (element - 1 === i) {
-				image.classList.add('show');
-				image.classList.remove('hide');
-			} else {
-				image.classList.add('hide');
-			}
-		})
-	}
-
-	function previousImageSlider(element) {
-		imageSlider.forEach((image, i) => {
-			if (element - 1 === i) {
-				image.classList.add('show');
-				image.classList.remove('hide');
-			} else {
-				image.classList.add('hide');
-			}
-		});
+	// Создаем новые элементы (точки) с помощью цикла
+	for (let i = 0; i < slides.length; i++) { // цикл (i меньше количества слайдов)
+		const dot = document.createElement('li'); // создаем точки (list-item)
+		dot.setAttribute('data-slide-to', i + 1); // устанавливаем data атрибут точкам со значением i + 1 (0 + 1) - чтобы потом его сопоставить с текущим слайдом на странице
+		dot.classList.add('dot'); // устанавливаем класс для точек
+		if (i == 0) { // условие (если i == 0 - первая точка)
+			dot.style.opacity = 1; // меняем класс прозрачности (делаем первый элемент активным)
+		}
+		dotsWrapper.append(dot); // устанавливаем точки внутрь нашего списка
+		dots.push(dot); // пушим в массив наши точки
 	};
 
-	
-	function defaultImage(el, i) {
-		imageSlider.forEach((item,i) => {
-			if (i === 0) {
-				spanCurrent.textContent = `0${1}`;
-				item.classList.add('show');
-				item.classList.remove('hide');
-			} else {
-				item.classList.add('hide');
-			}
+
+
+	next.addEventListener('click', (e) => {
+		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { // Условие (если ширина (переводим в number и отрезаем пиксели) умноженная на количество слайдов - 1 (это последний слайд в массиве слайдов, именно его номер - получается, что первый слайд идет в ширину 650px и когда смещаемся на 1950px, при следующем клике сработает наше условие) - "мы долистали до конца слайдера и пора вернуть его к первому слайду"
+			offset = 0; // тогда возвращаем карусель заново
+		} else { // Если не поледний слайд
+			offset += +width.slice(0, width.length - 2) // Добавляем offset (смещение) ширину без пикселей
+		}
+		slides.forEach(item => {
+			item.classList.remove('hide');
 		});
-	}
-	defaultImage();
+
+		slidesField.style.transform = `translateX(-${offset}px)`; // перемещаем карусель (вправо это будет -X (-650px)) - ориентируемся по оси Х (влево минус, вправо плюс)
+
+		if (slideIndex == slides.length) { // если текущий слайд будет равен последнему слайду
+			slideIndex = 1; // то переместимся на первый слайд
+		} else { // если не дошел до конца слайдера (не последий слайд)
+			slideIndex++; // то прибавляем slideIndex на 1
+		}
+
+		// Добавляем текущий слайд на страницу
+		currentOnPage();
+
+		// Добавление активности точкам в зависимости от кнопки на которую нажали
+		dotActive();
+	});
+
+
+	prev.addEventListener('click', () => {
+		if (offset == 0) { // Условие (если offset будет равен 0, то при клике на кнопку предыдущего слайдера будет срабатывать условие)
+			offset = +width.slice(0, width.length - 2) * (slides.length - 1); // тогда возвращаем слайдер в последний слайд (offset 1950px - cмещение на 1950px - на последний слайд)
+		} else { // Если не первый слайд
+			offset -= +width.slice(0, width.length - 2); // у offset (смещения) отнимаем ширину (- на - будет давать +)
+		}
+		slides.forEach(item => {
+			item.classList.remove('hide');
+		});
+
+		slidesField.style.transform = `translateX(-${offset}px)`; // Перемещаем карусель (Влево это будет - (-650px) - минус на минус плюс = +650px) 
+
+		if (slideIndex == 1) { // если у нас первый слайд
+			slideIndex = slides.length; // То меняем slideIndex на количетсво всех слайдов (то есть перемещаемся в конец слайдера)
+		} else { // Если не первый слайд
+			slideIndex--; // То мы уменьшаем наш slideIndex на 1
+		}
+
+		// Добавляем текущий слайд на страницу
+		currentOnPage();
+
+		// Добавление активности точкам в зависимости от кнопки на которую нажали
+		dotActive();
+
+	});
+
+	dots.forEach(dot => { // перебираем массив
+		dot.addEventListener('click', (e) => { // вешаем обработчик событий на отдельную точку
+			const slideTo = e.target.getAttribute('data-slide-to'); // помещаем в переменную (наше значение data-slide-to)
+	
+			slideIndex = slideTo; // меняем наш slideIndex на значение в data атрибуте (кликнули на 4 точку и в slideIndex пойдет значение 4)
+			offset = +width.slice(0, width.length - 2) * (slideTo - 1); // получение offset (ширину умножаем на slideTo - 1) - смещение на определенную ширину (нажали на 3 кнопку - ширину 650 * 2 = 1300px, на 4 кнопку - 650 * 3 = 1950px) 
+	
+			slidesField.style.transform = `translateX(-${offset}px)`; // добавляем смещение слайдера 
+	
+			// Добавляем текущий слайд на страницу
+			currentOnPage();
+	
+			// Добавление активности точкам в зависимости от кнопки на которую нажали
+			dotActive();
+		});
+	});
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
